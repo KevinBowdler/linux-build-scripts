cb910f7681fc38e69297bf19aeed67999f8a76e2
KevinBowdler <KevinBowdler@users.noreply.github.com>
2025-02-09 09:08:08 +0000

    Create meslo-fonts.sh

---
+++ meslo-fonts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+sudo mkdir -p /usr/share/fonts/truetype/meslo
+sudo mv MesloLGS\ NF\ *.ttf /usr/share/fonts/truetype/meslo
+sudo fc-cache -fv
